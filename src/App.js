8af1007ad327de025c75700bab2a02a975de2541
MdShafikulIslam <179398ssshofik@gmail.com>
2022-09-29 19:40:12 +0600

    3 question and answer is added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@
 import './App.css';
 import Gyms from './components/Gyms/Gyms';
 import Header from './components/Header/Header';
+import Question from './components/Question/Question';
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Gyms></Gyms>
+      <Question></Question>
     </div>
   );
 }
